49d9e299180391846012df5c550f9f7369082999
Pininkara <pininkara@outlook.com>
2022-01-21 12:38:24 +0800

    V1.1版本更新 1. 添加了起止页面 2. 修复大量bug 3. 添加提示信息 4. 添加图标

--- app/src/main/java/nnk/pininkara/morepic/MainActivity.java
+++ app/src/main/java/nnk/pininkara/morepic/MainActivity.java
@@ -1,26 +1,39 @@
 package nnk.pininkara.morepic;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import nnk.pininkara.morepic.utils.ImageData;
 import nnk.pininkara.morepic.utils.NetUtils;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    EditText tag_edit,num_edit,factor_edit,sub_edit;
-    Button tag_btn,start_btn;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText tag_edit, start_edit, end_edit, factor_edit, sub_edit;
+    Button tag_btn, start_btn, exit_btn;
+    TextView textView;
 
-    List<String> tags=new ArrayList<>();
+    String allTag = "";
+    String sub = "";
+    List<ImageData> list = new ArrayList<>();
+    int factor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,55 +42,132 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initView();
     }
 
-    private void initView(){
-        tag_edit=findViewById(R.id.tag_edit);
-        num_edit=findViewById(R.id.num_edit);
-        factor_edit=findViewById(R.id.factor_edit);
-        sub_edit=findViewById(R.id.sub_edit);
-        tag_btn=findViewById(R.id.tag_btn);
-        start_btn=findViewById(R.id.start_btn);
+    /**
+     * 初始化视图
+     */
+    private void initView() {
+        tag_edit = findViewById(R.id.tag_edit);
+        start_edit = findViewById(R.id.start_edit);
+        end_edit = findViewById(R.id.end_edit);
+        factor_edit = findViewById(R.id.factor_edit);
+        sub_edit = findViewById(R.id.sub_edit);
+        tag_btn = findViewById(R.id.tag_btn);
+        start_btn = findViewById(R.id.start_btn);
+        exit_btn = findViewById(R.id.exit_btn);
+        textView = findViewById(R.id.textView);
 
         tag_btn.setOnClickListener(this);
         start_btn.setOnClickListener(this);
+        exit_btn.setOnClickListener(this);
     }
 
 
     /**
      * Button点击事件监听
+     *
      * @param v
      */
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.tag_btn:
-               String tag=tag_edit.getText().toString().trim();
-                Log.d("nnk", "onClick: "+tag);
-               if (tag.equals("")){
-                   Toast.makeText(this, "TAG不能为空！", Toast.LENGTH_SHORT).show();
-               }else if(Pattern.matches("\\W+", tag)){
-                   Toast.makeText(this, "TAG只能有字母数字下划线", Toast.LENGTH_SHORT).show();
-               }else {
-                   tags.add(tag);
-                   Log.d("nnk223", "onClick: ");
-                   Toast.makeText(this, tag+"已添加！", Toast.LENGTH_SHORT).show();
-               }
+                String tag = tag_edit.getText().toString().trim();
+                Log.d("nnk", "onClick: " + tag);
+                if (tag.equals("")) {
+                    Toast.makeText(this, "TAG不能为空！", Toast.LENGTH_SHORT).show();
+                } else if (Pattern.matches("\\W+", tag)) {
+                    Toast.makeText(this, "TAG只能有字母数字下划线", Toast.LENGTH_SHORT).show();
+                } else {
+                    allTag = allTag + "+" + tag;
+                    Toast.makeText(this, tag + "已添加！\n" + "当前tag有:" + allTag, Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.start_btn:
-                int num= num_edit.getText().toString().trim().equals("") ?1:Integer.parseInt(num_edit.getText().toString().trim());
-                int factor= factor_edit.getText().toString().trim().equals("") ?100:Integer.parseInt(factor_edit.getText().toString().trim());
-                String sub= sub_edit.getText().toString().trim().equals("") ?String.valueOf(System.currentTimeMillis()):sub_edit.getText().toString().trim();
-                if (tags.size()!=0){
-                    Toast.makeText(this, "任务进行中，请等待~", Toast.LENGTH_SHORT).show();
-                        new Thread(()->{
+                int start_num = start_edit.getText().toString().trim().equals("") ? 1 : Integer.parseInt(start_edit.getText().toString().trim());
+                int end = end_edit.getText().toString().trim().equals("") ? 2 : Integer.parseInt(end_edit.getText().toString().trim());
+                factor = factor_edit.getText().toString().trim().equals("") ? 100 : Integer.parseInt(factor_edit.getText().toString().trim());
+                sub = sub_edit.getText().toString().trim().equals("") ? String.valueOf(System.currentTimeMillis()) : sub_edit.getText().toString().trim();
+                Log.d("nnk223", "start: "+start_num);
+                Log.d("nnk223", "end: "+end);
+                if (start_num <= end && start_num >= 1) {
+                    if (!allTag.equals("")) {
+                        Toast.makeText(this, "任务进行中，请等待~", Toast.LENGTH_SHORT).show();
+                        new Thread(() -> {
                             try {
-                                NetUtils.getResult(this,tags,num,factor,sub);
+                                list = NetUtils.getResponse(allTag, start_num, end);
+                                handler.sendEmptyMessage(1);
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
                         }).start();
-
+                    }
+                } else {
+                    Toast.makeText(this, "起止页码错误，请重新输入", Toast.LENGTH_SHORT).show();
                 }
+
                 break;
+            case R.id.exit_btn:
+                finish();
+                break;
+        }
+    }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what) {
+                case 1:
+                    Log.d("nnk223", "handleMessage: " + "已经完毕");
+                    textView.setText("检索到" + list.size() + "张图片，即将进行筛选\n");
+                    start_btn.setVisibility(View.GONE);
+                    tag_btn.setVisibility(View.INVISIBLE);
+                    processData();
+                    break;
+                case 2:
+                    Log.d("nnk223", "handleMessage: " + "筛选完毕");
+                    if (list.size() == 0) {
+                        textView.append("oops!没有筛选到符合要求的图片，请增加数量或减少质量因子");
+                        exit_btn.setVisibility(View.VISIBLE);
+                    } else {
+                        textView.append("筛选到" + list.size() + "张图片，正在添加下载任务\n");
+                        download();
+                    }
+
+                    break;
+                case 3:
+                    Log.d("nnk223", "handleMessage: 下载添加完成");
+                    textView.append("下载任务添加完成，您可以在通知中查看下载进度\n");
+                    textView.append("感谢您的使用~");
+                    exit_btn.setVisibility(View.VISIBLE);
+
+            }
+        }
+    };
+
+    private void processData() {
+        new Thread(() -> {
+            list = NetUtils.chosenData(list, factor);
+            handler.sendEmptyMessage(2);
+        }).start();
+    }
+
+    private void download() {
+        new Thread(() -> {
+            NetUtils.downloadMethod(this, list, sub);
+            handler.sendEmptyMessage(3);
+        }).start();
+    }
+
+    private void openAssignFolder(File file) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setDataAndType(Uri.fromFile(file), "file/*");
+        try {
+            startActivity(intent);
+//            startActivity(Intent.createChooser(intent,"选择浏览工具"));
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
--- app/src/main/java/nnk/pininkara/morepic/utils/NetUtils.java
+++ app/src/main/java/nnk/pininkara/morepic/utils/NetUtils.java
@@ -12,46 +12,50 @@ import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import cn.hutool.http.HttpRequest;
 
 public class NetUtils {
 
-
-    public static void getResult(Context context,List<String> tags,int num,int factor,String sub) throws IOException {
-        Log.d("nnk223", "getResult: "+num+"factor:"+factor);
-        List<ImageData> data = getResponse(tags,num);
-        List<ImageData> chosenData = chosenData(data,factor);
-        download(context,chosenData,sub);
-
-    }
-
-    private static List<ImageData> getResponse(List<String> tags,int num) throws IOException {
-        String param = "";
-        List<ImageData> data=new ArrayList<>();
-        for (String tag : tags) {
-            param = param + "+" + tag;
-        }
-        param = param.substring(1);
-        Log.d("nnk223", "getResponse: "+"https://yande.re/post?tags="+param);
-            String tempResponse = HttpRequest.get("https://yande.re/post?tags="+param)
+    /**
+     * 获得响应体
+     *
+     * @param allTag tag
+     * @param start    检索数量
+     * @return 图片列表
+     * @throws IOException 读取响应体的io异常
+     */
+    public static List<ImageData> getResponse(String allTag, int start,int end) throws IOException {
+        List<ImageData> data = new ArrayList<>();
+        allTag = allTag.substring(1);
+        for (int i = start; i <= end; i++) {
+            String url = "https://yande.re/post?page=" + i + "&tags=" + allTag;
+            Log.d("nnk223", "Url: " + url);
+            String tempResponse = HttpRequest.get(url)
                     .timeout(30000)
                     .execute().body();
             List<ImageData> imageData = getData(tempResponse);
-            if (!data.addAll(imageData)){
+            if (!data.addAll(imageData)) {
                 Log.d("nnk223", "getResponse: 出错了");
             }
-
-        Log.d("nnk223", "getResponse: "+data);
+        }
+        Log.d("nnk223", "getResponse: " + data.size());
         return data;
     }
 
+    /**
+     * 从response中获取图片信息
+     *
+     * @param response 响应体
+     * @return 图片信息
+     * @throws IOException 读取响应体的io异常
+     */
     private static List<ImageData> getData(String response) throws IOException {
         String line;
         List<String> json = new ArrayList<>();
@@ -72,42 +76,67 @@ public class NetUtils {
         for (String s : json) {
             data.add(gson.fromJson(s, ImageData.class));
         }
-        Log.d("nnk223", "getData: "+data);
         return data;
     }
 
-    private static List<ImageData> chosenData(List<ImageData> data,int factor){
-        List<ImageData> chosen=new ArrayList<>();
+    /**
+     * 筛选满足质量因子的图片
+     *
+     * @param data   图片列表
+     * @param factor 质量因子
+     * @return 筛选后的图片列表
+     */
+    public static List<ImageData> chosenData(List<ImageData> data, int factor) {
+        List<ImageData> chosen = new ArrayList<>();
         for (ImageData imageData : data) {
-            Log.d("nnk223", "Score: "+imageData.getScore());
-            if (imageData.getScore()>=factor){
+            Log.d("nnk223", "Score: " + imageData.getScore());
+            if (imageData.getScore() >= factor) {
                 chosen.add(imageData);
             }
         }
-        Log.d("nnk223", "chosenData: "+chosen);
+        Log.d("nnk223", "chosenData: " + chosen.size());
         return chosen;
     }
 
-    private static boolean download(Context context,List<ImageData> chosenData,String sub){
+    /**
+     * 下载文件至标准目录
+     *
+     * @param context    上下文
+     * @param chosenData 满足质量因子的图片列表
+     * @param sub        子目录名
+     */
+    public static void downloadMethod(Context context, List<ImageData> chosenData, String sub) {
+        File file=new File(Environment.DIRECTORY_PICTURES+"/morePic/"+sub+"/.nomedia");
+        Log.d("nnk223", "FilePath: "+file.getPath());
+        Log.d("nnk223", "exist: "+file.exists());
+        if (!file.exists()){
+            try {
+                Log.d("nnk223", "文件创建: "+file.createNewFile());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         for (ImageData data : chosenData) {
             String fileUrl = data.getFile_url();
-            Log.d("nnk223", "download: "+fileUrl);
+            Log.d("nnk223", "download: " + fileUrl);
             //创建下载任务,downloadUrl就是下载链接
             DownloadManager.Request request = new DownloadManager.Request(Uri.parse(fileUrl));
             //指定下载路径和下载文件名
-            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "/morePic/"+sub+"/"+fileUrl.substring(fileUrl.lastIndexOf("/") + 1));
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_PICTURES, "/morePic/" + sub + "/" + fileUrl.substring(fileUrl.lastIndexOf("/") + 1));
             request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
-            request.setTitle("下载中~");
+            request.setTitle("morePic");
             request.setDescription("morePic正在下载");
             request.setAllowedOverRoaming(false);
 //            //设置文件存放目录
 //            request.setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, "");
             //获取下载管理器
-            DownloadManager downloadManager= (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+            DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
             //将下载任务加入下载队列，否则不会进行下载
             downloadManager.enqueue(request);
         }
-        return true;
     }
+
+
+
 }
 
